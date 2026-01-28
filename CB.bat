a6855b2105a69f509be56f295b0d4a740863b772
jin <149501717+monlol@users.noreply.github.com>
2026-01-28 22:54:32 +0700

    Add files via upload

--- CB.bat
+++ CB.bat
@@ -1,27 +1,19 @@
 @echo off
-title Crash Browser
-mode con lines=5 cols=40
-
-:: Website
 set keywords=poki gamevui crazygames
 
-:watchdog
-for %%a in (%keywords%) do (
-    tasklist /v /fi "STATUS eq running" | findstr /i "%%a" >nul
-    if not errorlevel 1 (
-        taskkill /f /im zen.exe >nul 2>&1
-        taskkill /f /im chromium.exe >nul 2>&1
-        taskkill /f /im browser.exe >nul 2>&1
-        taskkill /f /im msedge.exe >nul 2>&1
-        taskkill /f /im firefox.exe >nul 2>&1
-        taskkill /f /im brave.exe >nul 2>&1
-        taskkill /f /im thorium.exe >nul 2>&1
-        
-        cls
-        echo hehe
-        timeout /t 3 >nul
-    )
+tasklist /v /fi "STATUS eq running" | findstr /i "%keywords%" >nul
+
+if %errorlevel% == 0 (
+    taskkill /f /im chrome.exe >nul 2>&1
+	taskkill /f /im browser.exe >nul 2>&1
+	taskkill /f /im browsers.exe >nul 2>&1
+	taskkill /f /im zen.exe >nul 2>&1
+    taskkill /f /im chromium.exe >nul 2>&1
+    taskkill /f /im msedge.exe >nul 2>&1
+    taskkill /f /im thorium.exe >nul 2>&1
+    taskkill /f /im brave.exe >nul 2>&1
+    
+    exit /b 1
 )
 
-timeout /t 2 /nobreak >nul
-goto watchdog
+exit /b 0
